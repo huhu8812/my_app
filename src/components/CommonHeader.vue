8e2c59262a7117cf7de801b035d5e2f24d7cd2b3
huhu8812 <252077095@qq.com>
2023-03-20 20:11:07 +0800

    0320

--- src/components/CommonHeader.vue
+++ src/components/CommonHeader.vue
@@ -2,8 +2,12 @@
   <div class="header-container">
     <div class="l-content">
       <el-button @click="handleMenu" icon="el-icon-menu" size="mini"></el-button>
-      <span class="text">首页</span>
-    </div>
+      <el-breadcrumb separator="/">
+        <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item><a href="/">活动管理</a></el-breadcrumb-item>
+        <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+        <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+      </el-breadcrumb>    </div>
     <div class="r-content">
       <el-dropdown>
         <span class="el-dropdown-link">
